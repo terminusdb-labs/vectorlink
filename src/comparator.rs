4e61120795178d6afe8a08c38d0ce06212b8729c
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-12-18 13:23:30 +0100

    keep loaded vecs around long enough to do comparison

--- src/comparator.rs
+++ src/comparator.rs
@@ -15,15 +15,23 @@ pub struct OpenAIComparator {
 
 impl Comparator<Embedding> for OpenAIComparator {
     fn compare_vec(&self, v1: AbstractVector<Embedding>, v2: AbstractVector<Embedding>) -> f32 {
+        #[allow(unused_assignments)]
+        let mut v1_opt = None;
+        #[allow(unused_assignments)]
+        let mut v2_opt = None;
         let v1 = match v1 {
             AbstractVector::Stored(vid) => {
-                &*self.store.get_vec(&self.domain, vid.0).unwrap().unwrap()
+                let loaded = self.store.get_vec(&self.domain, vid.0).unwrap().unwrap();
+                v1_opt = Some(loaded);
+                &**v1_opt.as_ref().unwrap()
             }
             AbstractVector::Unstored(v) => v,
         };
         let v2 = match v2 {
             AbstractVector::Stored(vid) => {
-                &*self.store.get_vec(&self.domain, vid.0).unwrap().unwrap()
+                let loaded = self.store.get_vec(&self.domain, vid.0).unwrap().unwrap();
+                v2_opt = Some(loaded);
+                &**v2_opt.as_ref().unwrap()
             }
             AbstractVector::Unstored(v) => v,
         };
