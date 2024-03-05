36a81cb787a910a54263723f2418cf31eb65b271
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-03-05 14:15:33 +0100

    centroid 32 comparator that serializes to raw vecs

--- vectorlink/src/comparator.rs
+++ vectorlink/src/comparator.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
+use std::os::unix::fs::MetadataExt;
 use std::{path::Path, sync::Arc};
 
 use parallel_hnsw::{AbstractVector, Comparator, Serializable, SerializationError, VectorId};
 
+use crate::vecmath::{self, Centroid32, CENTROID_32_BYTE_LENGTH};
 use crate::vectors::LoadedVec;
 use crate::{
     vecmath::{normalized_cosine_distance, Embedding},
@@ -68,6 +70,57 @@ impl Serializable for OpenAIComparator {
         Ok(OpenAIComparator {
             domain,
             store: store.into(),
+        })
+    }
+}
+
+#[derive(Clone)]
+pub struct Centroid32Comparator {
+    centroids: Arc<Vec<Centroid32>>,
+}
+
+impl Comparator for Centroid32Comparator {
+    type T = Centroid32;
+
+    type Borrowable<'a> = &'a Centroid32;
+
+    fn lookup(&self, v: VectorId) -> Self::Borrowable<'_> {
+        &self.centroids[v.0]
+    }
+
+    fn compare_raw(&self, v1: &Self::T, v2: &Self::T) -> f32 {
+        vecmath::normalized_cosine_distance_32(v1, v2)
+    }
+}
+
+impl Serializable for Centroid32Comparator {
+    type Params = ();
+
+    fn serialize<P: AsRef<Path>>(&self, path: P) -> Result<(), SerializationError> {
+        let buf: &[u8] = unsafe {
+            std::slice::from_raw_parts(
+                self.centroids.as_ptr() as *const u8,
+                self.centroids.len() * std::mem::size_of::<Centroid32>(),
+            )
+        };
+        std::fs::write(path, buf)?;
+        Ok(())
+    }
+
+    fn deserialize<P: AsRef<Path>>(
+        path: P,
+        _params: Self::Params,
+    ) -> Result<Self, SerializationError> {
+        let size = std::fs::metadata(&path)?.size() as usize;
+        assert_eq!(0, size % CENTROID_32_BYTE_LENGTH);
+        let number_of_centroids = size / CENTROID_32_BYTE_LENGTH;
+        let mut vec = vec![Centroid32::default(); number_of_centroids];
+        let mut file = std::fs::File::open(&path)?;
+        let buf = unsafe { std::slice::from_raw_parts_mut(vec.as_mut_ptr() as *mut u8, size) };
+        file.read_exact(buf)?;
+
+        Ok(Self {
+            centroids: Arc::new(vec),
         })
     }
 }
